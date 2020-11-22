61c0682a8df8710d0a2840e446cdfd4ca79f79b3
Ronan <galv.ronan@gmail.com>
2020-11-21 21:35:37 -0500

    feature/list-issues completed

--- assets/js/single.js
+++ assets/js/single.js
@@ -4,14 +4,26 @@ var queryString = document.location.search;
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+
 }
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was succesful
         if (response.ok) {
@@ -19,14 +31,14 @@ var getRepoIssues = function(repo) {
                 // pass response data to DOM function
                 displayIssues(data);
 
-                // check if api has paginated issues
+                // check if api has paginated issues (more than 30 issues)
                 if (response.headers.get("Link")) {
                     displayWarning(repo);
                 }
             });
-        }
-        else {
-            alert("There was a problem with your request!");
+        } else {
+            // if not succesful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 }
